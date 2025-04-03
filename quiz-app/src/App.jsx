f290a80418242ae1768d32f5538072b6e8056948
TerenceCLZhang <terence.zhang2014@gmail.com>
2025-04-04 02:51:10 +1300

    project added

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import questionsData from "./questions.json";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [currentCorrect, setCurrentCorrect] = useState(0);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [showCorrect, setShowCorrect] = useState(false);
+
+  const question = questionsData.questions[questionIndex];
+
+  const handleSubmit = () => {
+    checkAnswer();
+    goToNextQuestion();
+  };
+
+  const checkAnswer = () => {
+    if (selectedAnswer === question.answer) {
+      setCurrentCorrect((prev) => prev + 1);
+    }
+  };
+
+  const goToNextQuestion = () => {
+    if (questionIndex < questionsData.questions.length - 1) {
+      setQuestionIndex((prev) => prev + 1);
+      setSelectedAnswer(null);
+    } else {
+      setShowCorrect(true);
+    }
+  };
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="main-content">
+      <div className="question-container">
+        {showCorrect ? (
+          <h1>
+            Score: {currentCorrect}/{questionsData.questions.length}
+          </h1>
+        ) : (
+          <>
+            <h1>{question.question}</h1>
+            <ul className="options">
+              {question.options.map((element) => (
+                <li key={element}>
+                  <input
+                    type="radio"
+                    name="option"
+                    id={`option_${element}`}
+                    value={element}
+                    checked={selectedAnswer === element}
+                    onChange={(e) => setSelectedAnswer(e.target.value)}
+                  />
+                  <label htmlFor={`option_${element}`}>{element}</label>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <input
+        type="button"
+        value={showCorrect ? "Reload" : "Submit"}
+        onClick={showCorrect ? reloadPage : handleSubmit}
+        className="btn"
+      />
+    </main>
+  );
 }
 
-export default App
+export default App;
